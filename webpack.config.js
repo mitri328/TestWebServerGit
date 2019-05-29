57f65593ccdbd6e0ebf4311758981d2526ca32ca
jeand1 <dimitri.jeanneret@students.bfh.ch>
2019-05-29 23:49:15 +0200

    add bundle hash

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,17 @@
 /* global module, require, __dirname*/
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[chunkhash].js',
         path: path.resolve(__dirname, 'dist')
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({template: 'src/index.html'})
+    ]
+
 };
